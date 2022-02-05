85c95e757917de017c559f4dfc634f2beba824dc
Hudson Dean <hudsonxy@gmail.com>
2022-02-05 13:28:35 -0700

    get angular material card and button work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const material = [
+  MatCardModule,
+  MatButtonModule,
+];
 
 @NgModule({
   declarations: [
@@ -10,7 +18,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    material,
   ],
   providers: [],
   bootstrap: [AppComponent]
